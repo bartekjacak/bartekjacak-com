de07be22cbbb852ab510eaea3ebdfe8ade96d134
Bartłomiej Jacak <bartek.jacak@gmail.com>
2025-02-24 00:15:37 +0700

    feat: add old blog post

--- src/content/usePosts.ts
+++ src/content/usePosts.ts
@@ -13,5 +13,11 @@ export function usePosts(): Post[] {
       date: new Date("2024-02-14").toISOString(),
       displayDate: "14th Feb 2024",
     },
+    {
+      url: "https://medium.com/@bartekjacak/how-not-to-do-any-long-term-challenge-d5f10304bedb",
+      name: "How not to do any long-term challenge",
+      date: new Date("2018-12-23").toISOString(),
+      displayDate: "23rd Dec 2018",
+    },
   ];
 }
